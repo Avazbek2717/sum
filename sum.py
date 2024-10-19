f7b8eda42b0ac3bce1823a9fd6f333b0c620613b
Avazbek <ax1of710@gmail.com>
2024-10-19 14:38:00 +0500

    paskal

--- sum.py
+++ sum.py
@@ -4,18 +4,18 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
-        def dfs(node, curSum):
-            if not node:
-                return False
-            curSum = curSum + node.val
-            if not node.left and not node.right:
-                return curSum == targetSum
+# class Solution:
+#     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+#         def dfs(node, curSum):
+#             if not node:
+#                 return False
+#             curSum = curSum + node.val
+#             if not node.left and not node.right:
+#                 return curSum == targetSum
             
-            return dfs(node.left, curSum) or dfs(node.right, curSum)
+#             return dfs(node.left, curSum) or dfs(node.right, curSum)
         
-        return dfs(root, 0)
+#         return dfs(root, 0)
 
 
             
@@ -31,4 +31,22 @@ class Solution:
 # root.right.right = TreeNode(4)
 # root.right.right.right = TreeNode(1)
 # targetSum  = 22
+
+
+
+
+#118- misol
+
+# class Solution:
+#     def generate(self, numRows: int) -> list[list[int]]:
+#         t = [[1]]
+#         for i in range(numRows-1):
+#             tempt = [0] + t[-1] +[0]
+#             row = []
+#             for j in range(len(t[-1])+1):
+#                 row.append(tempt[j]+tempt[j+1])
+#             t.append(row)
+#         return  t
+
+
 
